98a52cef338fd63c2516813acd5fab78256a7cd6
beykito <nusamanoban@gmail.com>
2024-10-13 23:35:54 +0700

    Lesgo kita niat brow
    Icad makan ikan nemoo

---
+++ Assets/Game/Code/Manager/Tweener/TweenerAds.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+using DG.Tweening;
+
+namespace Beyaka.Manager
+{
+    public class TweenerAds : MonoBehaviour
+    {
+        [SerializeField] private float center, duration;
+        [SerializeField] private Transform firstpos, point;
+
+        private Vector3 startPos;
+
+        private void Start()
+        {
+            startPos = firstpos.position;
+        }
+
+        public void popup(Transform popup)
+        {
+            popup.DOMove(point.position, duration).SetEase(Ease.InExpo);
+        }
+        public void BackPopup(Transform popup)
+        {
+            popup.DOMove(startPos, duration).SetEase(Ease.InExpo);
+        }
+
+    }
+}
